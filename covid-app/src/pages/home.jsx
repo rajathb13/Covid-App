499d63d703485d86ee3044fa9caf34f86662e113
Rajath Bharadwaj <rajathb13@gmail.com>
2020-05-24 19:09:31 +0530

    commit5

---
+++ covid-app/src/pages/home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Global from "./Global";
+import Charts from "../components/Charts";
+import CountryPicker from "../components/CountryPicker";
+import { fetchData } from "../api/ApiService";
+
+class Home extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    console.log(fetchedData);
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+    console.log(fetchedData);
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <React.Fragment>
+        <Global data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
